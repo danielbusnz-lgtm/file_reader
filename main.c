90990d131d9aa73f76ed64cb7530235294be280d
danielbusnz-lgtm <danielbusnz@gmail.com>
2026-04-28 14:32:54 -0400

    adding support for multiple files

--- main.c
+++ main.c
@@ -2,38 +2,40 @@
 #include <ctype.h>
 
 int main(int argc, char *argv[]) {
-    int counter = 0;
-    int isInside = 0;
-    int c;
-    int charCounter = 0;
-    int lineCounter = 0;
+    for ( int i = 1; i < argc; i++){
+
+        int counter = 0;
+        int isInside = 0;
+        int c;
+        int charCounter = 0;
+        int lineCounter = 0;
 
 
-    FILE *file = fopen(argv[1], "r");
+        FILE *file = fopen(argv[i], "r");
 
-    if (file == NULL){
-        printf("couldnt open file\n");
-        return 1;
-    }
+        if (file == NULL){
+            printf("couldnt open file\n");
+            continue;
+        }
 
-    while ((c = fgetc(file)) != EOF){
-         if (c == '\n'){
-             lineCounter ++;
-         }
-         if (isspace(c)){
-             isInside = 0;
-         
-        } else {
-             charCounter ++;
-             if (isInside == 0){
-                 counter ++;
-                 isInside = 1;
+        while ((c = fgetc(file)) != EOF){
+             if (c == '\n'){
+                 lineCounter ++;
              }
+             if (isspace(c)){
+                 isInside = 0;
+             
+            } else {
+                 charCounter ++;
+                 if (isInside == 0){
+                     counter ++;
+                     isInside = 1;
+                 }
+        }
+        }
+        
+        fclose(file); 
+        printf("%s: %d words, %d characters, %d lines\n",argv[i], counter, charCounter, lineCounter);
     }
-    }
-    printf("%d words\n", counter);
-    printf("%d characters\n", charCounter);
-    printf("%d Lines\n", lineCounter);
-    fclose(file); 
     return 0;
 }
