cb72523cffb44d0243733821a4bfcc0334500d03
danielbusnz-lgtm <danielbusnz@gmail.com>
2026-04-28 15:26:43 -0400

    fixed some buggy

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 
 int main(int argc, char *argv[]) {
     for ( int i = 1; i < argc; i++){
-
+        char buffer[100];
+        int bufferIndex = 0;
         int counter = 0;
         int isInside = 0;
         int c;
@@ -19,21 +20,31 @@ int main(int argc, char *argv[]) {
         }
 
         while ((c = fgetc(file)) != EOF){
-             if (c == '\n'){
-                 lineCounter ++;
-             }
+             if (c == '\n'){lineCounter ++;}
+
              if (isspace(c)){
-                 isInside = 0;
-             
+                 if (isInside ==1 ){
+                 buffer[bufferIndex] = '\0';
+                 printf("word: %s\n", buffer);
+                 bufferIndex = 0;
+             }
+             isInside = 0;
             } else {
                  charCounter ++;
+                 buffer[bufferIndex] = c;
+                 bufferIndex ++;
+
                  if (isInside == 0){
                      counter ++;
                      isInside = 1;
-                 }
+                   }
+             }
         }
+        if (isInside == 1){
+            buffer[bufferIndex] = '\0';
+            printf("word: %s\n", buffer);
         }
-        
+
         fclose(file); 
         printf("%s: %d words, %d characters, %d lines\n",argv[i], counter, charCounter, lineCounter);
     }
